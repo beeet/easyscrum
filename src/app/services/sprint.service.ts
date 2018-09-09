13b266ad072d6b245e7ed25c79caf13ef3165feb
BeatBaumann <beat.baumann@gmx-topmail.de>
2018-09-09 16:37:32 +0200

    cleanup

--- src/app/services/sprint.service.ts
+++ src/app/services/sprint.service.ts
@@ -52,8 +52,9 @@ export class SprintService implements Crud<Sprint> {
     const now = this.dateUtil.now();
     return this.sprints.filter(s => {
       return isBefore(s.begin, now) || isEqual(s.begin, now);
-  });
-    
+    });
+  }
+
   getNext(): Sprint {
     const current = this.getCurrent();
     const nextSprints = this.sprints.filter(s => isAfter(s.begin, current.end));
