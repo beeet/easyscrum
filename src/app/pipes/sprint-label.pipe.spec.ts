e292cae42badcbdd4a32e508cec359712ca8b034
beeet <beatschumacher@bluewin.ch>
2018-09-29 11:14:17 +0200

    add github pages script

--- src/app/pipes/sprint-label.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SprintLabelPipe } from './sprint-label.pipe';
-
-describe('SprintLabelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SprintLabelPipe(null);
-    expect(pipe).toBeTruthy();
-  });
-});
